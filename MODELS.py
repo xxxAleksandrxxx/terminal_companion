1a2e9a2f7ebf803cb7d57d4172890248017b3a46
Aleksandr D <none>
2024-01-30 11:33:50 +0700

    add MODELS.py with models (move models from main.py)

---
+++ MODELS.py
@@ -0,0 +1,11 @@
+'''
+Keep models here. 
+Key - short model name by which it will be called from user prompt
+Value - full model name; link for reference: https://platform.openai.com/docs/models 
+'''
+models = {
+    # "gpt3": "gpt-3.5-turbo-0613",
+    # "gpt4": "gpt-4-1106-preview",
+    "gpt3": "gpt-3.5-turbo-1106",  # released in the end of Jan 2024
+    "gpt4": "gpt-4-0125-preview"   # released in the end of Jan 2024
+}
