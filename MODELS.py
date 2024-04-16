650297d49d1163dcfc6ec974ce596cc9def870d9
Aleksandr D <none>
2024-04-16 17:50:46 +0700

    add model gpt-4-turbo-2024-04-09 and change values for gpt3 with gpt4 to gpt-3.5-turbo and gpt-4-turbo accordingly

--- MODELS.py
+++ MODELS.py
@@ -4,8 +4,11 @@ Key - short model name by which it will be called from user prompt
 Value - full model name; link for reference: https://platform.openai.com/docs/models 
 '''
 models = {
+    "gpt4": "gpt-4-turbo",  # Apr 2024: points to gpt-4-turbo-2024-04-09
+    "gpt3": "gpt-3.5-turbo",  # Points to gpt-3.5-turbo-0125.
     # "gpt3": "gpt-3.5-turbo-0613",
     # "gpt4": "gpt-4-1106-preview",
-    "gpt3": "gpt-3.5-turbo-1106",  # released in the end of Jan 2024
-    "gpt4": "gpt-4-0125-preview"   # released in the end of Jan 2024
+    # "gpt3": "gpt-3.5-turbo-1106",  # released in the end of Jan 2024
+    # "gpt4": "gpt-4-0125-preview",   # released in the end of Jan 2024
+    # "gpt4": "gpt-4-turbo-2024-04-09",   # released in the end of Apr 2024
 }
